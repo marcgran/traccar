db3cf17d130e1af6a0de950c831c3a809148c4f8
Marc Gran <marc@grantech.es>
2026-06-07 20:43:04 +0200

    fix: correct copyright headers - new files authored by Marc Gran, not Anton Tananaev

--- src/main/java/org/traccar/api/resource/GeocoderResource.java
+++ src/main/java/org/traccar/api/resource/GeocoderResource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Anton Tananaev (anton@traccar.org)
+ * Copyright 2024 Marc Gran (marc@grantech.es)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
